ad41af29acea7190e10c58d04a00253a92906b9a
msomele <mesomoza@movistar.es>
2020-04-12 05:55:25 +0200

    Hammer Added
    with emission controller. Organized Players folder.

---
+++ Assets/Players/Barbarian/Scripts/HammerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerController : MonoBehaviour
+{
+
+    public Material mat;
+    public float intensity = 3;
+    Color baseColor = Color.white;
+   
+    void Update()
+    {
+        
+        Color finalColor = baseColor * Mathf.LinearToGammaSpace(intensity);
+        mat.SetColor("_EmissionColor", finalColor);
+
+    }
+
+   //cuando se haga la animación de hit, cambiar como se ha hecho en el default pulse a una animación el parametro intensity
+}
